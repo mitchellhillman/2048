e413fa2e9240a14cd855fc3a96220de1a1eef8d1
Mitchell Hillman <mitchell.hillman@willowtreeapps.com>
2023-12-20 15:04:49 -0500

    Cell loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    NgIf
+  ],
   templateUrl: `./app.component.html`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Foobar';
+  title = '2048';
+
+  cells = [
+    2, 4, 0, 0,
+    2, 4, 0, 0,
+    2, 4, 0, 0,
+    2, 4, 0, 0,
+  ]
+
 }
