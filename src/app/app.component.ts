a8c8253e64666382ee302799a7b709c142079bb1
Mitchell Hillman <mitchell.hillman@willowtreeapps.com>
2023-12-20 14:30:55 -0500

    Create template file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `<h1>Hello world!</h1>`,
+  templateUrl: `./app.component.html`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
+  title = 'Foobar';
 }
