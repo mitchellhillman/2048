d46643810c7d89641a886399c8916884d67c376d
Mitchell Hillman <mitchell.hillman@willowtreeapps.com>
2023-12-21 10:56:22 -0500

    Add keyboard controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -20,4 +19,52 @@ export class AppComponent {
     2, 4, 0, 0,
     2, 4, 0, 0,
   ]
+
+  moveUp() {
+    console.log('UP')
+  }
+
+  moveLeft() {
+    console.log('LEFT')
+  }
+
+  moveDown() {
+    console.log('DOWN')
+  }
+
+  moveRight() {
+    console.log('RIGHT')
+  }
+
+  @HostListener('document:keydown', ['$event']) 
+  handleKeyboardEvent({key}: KeyboardEvent) {
+    switch(key) {
+      case 'ArrowUp':
+         this.moveUp()
+         return;
+      case 'ArrowLeft':
+         this.moveLeft()
+         return;
+      case 'ArrowDown':
+         this.moveDown()
+         return;
+      case 'ArrowRight':
+         this.moveRight()
+         return;
+      case 'w':
+         this.moveUp()
+         return;
+      case 'a':
+         this.moveLeft()
+         return;
+      case 's':
+         this.moveDown()
+         return;
+      case 'd':
+         this.moveRight()
+         return;
+      default: 
+         return;
+    }
+  }
 }
