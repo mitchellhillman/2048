531bf5bae9980c02863057267294e6f824e9b2cd
Mitchell Hillman <mitchell.hillman@willowtreeapps.com>
2023-12-27 12:19:04 -0500

    adjust colors

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export default  {
+    BLANKBOARD: [
+        0,0,0,0,
+        0,0,0,0,
+        0,0,0,0,
+        0,0,0,0,
+    ],
+    NODIFF: [
+        false,false,false,false,
+        false,false,false,false,
+        false,false,false,false,
+        false,false,false,false,
+    ]
+}
